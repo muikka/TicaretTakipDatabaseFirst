4f4911d1a6b211c136ae7d17416a7ad1d58ccadd
muikka <muhammed.ikbal.karakaya@gmail.com>
2016-12-26 20:29:44 +0300

    Add project files.

---
+++ TicaretTakipDatabaseFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicaretTakipDatabaseFirst.Startup))]
+namespace TicaretTakipDatabaseFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
